e0da20be5acfe8e17eefff47d72875e5a8ea54c3
sthancock <steven.hancock@ed.ac.uk>
2021-01-20 10:03:51 +0000

    SImple sort made robust for cases where array is aleady sorted.

--- week1/sort/simpSort.py
+++ week1/sort/simpSort.py
@@ -14,6 +14,7 @@ from datetime import datetime
 def findMin(arr):
   '''A minimum fuinding function'''
   minN=10000  # a big number
+  minInd=0
   for i in range(0,arr.shape[0]):
     if(arr[i]<minN):
       minN=arr[i]
